1bd9f24f8643b100b4faab20379e6c86d37c54aa
Vishal Tanna <vishal.tanna2010@gmail.com>
2023-10-05 14:31:50 +0530

    Added env file

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -7,11 +7,14 @@ export const USER_AVATAR =
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
+console.log(process.env.REACT_APP_TMDB_KEY);
+
 export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNzJjY2VmNDc2YTQxNjZmNzM5OGZkMzVlNTYwZmM3NyIsInN1YiI6IjY1MWQ0ZjFmMDcyMTY2MDBjNTY4ZGQwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YOPp-oxLAyDEicbEBxsPDwybm1gyVGsyXhhHq1E0ZqE",
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
   },
 };
