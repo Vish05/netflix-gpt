9073961f45b05391378e363dbd7615646d60d219
Vishal Tanna <vishal.tanna2010@gmail.com>
2023-10-09 18:18:22 +0530

    Changes in Readme and GPT Search

--- src/component/GptSearchBar.js
+++ src/component/GptSearchBar.js
@@ -1,16 +1,42 @@
+import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import openai from "../utils/openai";
 import lang from "../utils/languageConstants";
 const GptSearchBar = () => {
   const langKey = useSelector((store) => store.config.lang);
+
+  const searchText = useRef(null);
+
+  const handleGptSearchClick = async () => {
+    console.log(searchText.current.value);
+    // Make an API call to GPT API and get Movie Results
+
+    const gptResults = await openai.chat.completions.create({
+      messages: [{ role: "user", content: "Say this is test" }],
+      model: "gpt-3.5-turbo",
+    });
+
+    if (!gptResults.choices) {
+      // TODO: Write Error Handling
+    }
+  };
+
   return (
     <div className="pt-[10%] flex justify-center">
-      <form className="w-1/2 bg-black grid grid-cols-12">
+      <form
+        className="w-1/2 bg-black grid grid-cols-12"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
           type="text"
+          ref={searchText}
           className="p-4 m-4 col-span-9"
           placeholder={lang[langKey].gptSearchPlaceholder}
         />
-        <button className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg">
+        <button
+          className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded-lg"
+          onClick={handleGptSearchClick}
+        >
           {lang[langKey].search}
         </button>
       </form>
